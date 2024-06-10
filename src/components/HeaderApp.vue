3ff7979ac2bf7f3d01cc0751ab7a3cffbabbfa7f
MatteoNeri99 <matteolavoro99@gmail.com>
2024-06-10 16:28:41 +0200

    inseriti link nel header con un v-for

--- src/components/HeaderApp.vue
+++ src/components/HeaderApp.vue
@@ -2,6 +2,69 @@
 export default{
   data() {
       return {
+
+        headerLinks:[
+          {
+            title: 'charaters',
+            link:'#',
+            id: 1
+
+          },
+          {
+            title: 'comics',
+            link:'#',
+            id: 2
+
+          },
+          {
+            title: 'movies',
+            link:'#',
+            id: 3
+
+          },
+          {
+            title: 'tv',
+            link:'#',
+            id: 4
+
+          },
+          {
+            title: 'games',
+            link:'#',
+            id: 5
+
+          },
+          {
+            title: 'collectibles',
+            link:'#',
+            id: 6
+
+          },
+          {
+            title: 'videos',
+            link:'#',
+            id: 7
+
+          },
+          {
+            title: 'fans',
+            link:'#',
+            id: 8
+
+          },
+          {
+            title: 'news',
+            link:'#',
+            id: 9
+
+          },
+          {
+            title: 'shop',
+            link:'#',
+            id: 10
+
+          }
+        ]
         
       }
     }
@@ -12,9 +75,44 @@ export default{
 </script>
 
 <template>
- <h1>header</h1>
+
+  <header>
+
+    <img src="../assets/img/dc-logo.png" alt="">
+
+    <nav>
+      <ul>
+
+        <li  v-for="(element, index) in headerLinks" key="element.id"><a :href="element.link">{{ element.title }}</a> </li>
+      
+      </ul>
+
+    </nav>
+
+</header>
+ 
 </template>
 
 <style lang="scss" scoped>
+
+header{
+  display: flex;
+
+  ul{
+    display: flex;
+    list-style-type: none;
+
+    li{
+      margin-right: 2rem;
+
+      a{
+        text-decoration: none;
+        color: black;
+        text-transform: uppercase;
+      }
+    }
+  }
+}
+
 
 </style>
